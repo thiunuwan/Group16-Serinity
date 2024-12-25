c3f30fff7bd1029ed339b70130d0a291c2ee3403
Ashen <Ashen.Kavishka@cloudsolutions.com.sa>
2024-12-25 14:05:48 +0530

    Added update tests

---
+++ src/test/java/starter/steps/UpdateBookByIdSteps.java
@@ -0,0 +1,95 @@
+package starter.steps;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.restassured.response.Response;
+import net.serenitybdd.core.Serenity;
+import net.serenitybdd.rest.SerenityRest;
+import net.thucydides.core.util.EnvironmentVariables;
+import net.thucydides.core.util.SystemEnvironmentVariables;
+import starter.hooks.BookLifecycleHooks;
+import starter.utils.AuthUtils;
+
+import static net.serenitybdd.rest.SerenityRest.restAssuredThat;
+import static org.junit.Assert.assertTrue;
+
+public class UpdateBookByIdSteps {
+    // Retrieve BASE_URL from configuration
+    private final EnvironmentVariables environmentVariables = SystemEnvironmentVariables.createEnvironmentVariables();
+    private final String BASE_URL = environmentVariables.getProperty("api.base.url", "http://localhost:8080/api");
+    private static final int testBookId = BookLifecycleHooks.getTestBookId();
+    @When("I send a PUT request to update the test book")
+    public void iSendAPUTRequestToUpdateTheTestBook() {
+        // Update admin credentials from session variables
+        String username = Serenity.sessionVariableCalled("username");
+        String password = Serenity.sessionVariableCalled("password");
+
+        // Generate Basic Auth header
+        String basicAuthHeader = AuthUtils.generateBasicAuthHeader(username, password);
+
+        // JSON body for the PUT request
+        String requestBody = "{"
+                + "\"title\": \"Updated Book Title\","
+                + "\"author\": \"Updated Author Name\","
+                + "}";
+
+        // Send PUT request to update the book with ID 1
+        SerenityRest.given()
+                .header("Authorization", basicAuthHeader)
+                .body(requestBody)
+                .put(BASE_URL +"/books/"+ testBookId);
+    }
+    @When("I send a PUT request to update a book with a non-existent ID {int}")
+    public void iSendAPUTRequestToUpdateABookWithANonExistentIDBook_id(int nonExistentBookID) {
+        // Retrieve admin credentials from session variables
+        String username = Serenity.sessionVariableCalled("username");
+        String password = Serenity.sessionVariableCalled("password");
+
+        // Generate Basic Auth header
+        String basicAuthHeader = AuthUtils.generateBasicAuthHeader(username, password);
+
+        // JSON body for the PUT request
+        String requestBody = "{"
+                + "\"title\": \"Non-existent Book Title\","
+                + "\"author\": \"Non-existent Author Name\","
+                + "}";
+        // Send PUT request to update the book with ID 1
+        SerenityRest.given()
+                .header("Authorization", basicAuthHeader)
+                .body(requestBody)
+                .put(BASE_URL +"/books/"+nonExistentBookID);
+    }
+    @When("I send a PUT request to update the test book with missing mandatory fields")
+    public void iSendAPUTRequestToUpdateTheTestBookWithMissingMandatoryFields() {
+        // Update admin credentials from session variables
+        String username = Serenity.sessionVariableCalled("username");
+        String password = Serenity.sessionVariableCalled("password");
+
+        // Generate Basic Auth header
+        String basicAuthHeader = AuthUtils.generateBasicAuthHeader(username, password);
+
+        // JSON body for the PUT request with missing mandatory fields
+        String requestBody = "{}"; // Empty JSON body, simulating missing fields
+
+        // Send PUT request to update the book
+        SerenityRest.given()
+                .header("Authorization", basicAuthHeader)
+                .body(requestBody)
+                .put(BASE_URL + "/books/" + testBookId);
+    }
+    @Then("the response status code should be {int}")
+    public void theResponseStatusCodeShouldBe(int statusCode) {
+        restAssuredThat(response -> response.statusCode(statusCode));
+    }
+    @And("the response should contain the error message {string}")
+    public void theResponseShouldContainTheErrorMessage(String expectedErrorMessage) {
+        // Retrieve the response body as a String
+        Response response = SerenityRest.lastResponse();
+        String responseBody = response.getBody().asString();
+
+        // Verify that the response body contains the expected error message
+        assertTrue("Response body should contain the error message: " + expectedErrorMessage,
+                responseBody.contains(expectedErrorMessage));
+    }
+}
