fa2ad58490cb8c9e25bad92d228b55a4d10e36a3
Milanka00 <milankatharangana01@gmail.com>
2024-12-26 15:54:25 +0530

    UIT1: Actions structure

---
+++ src/test/java/starter/actions/RecruitmentCandidateActionSteps.java
@@ -0,0 +1,25 @@
+package starter.actions;
+
+import net.serenitybdd.core.steps.UIInteractionSteps;
+import net.thucydides.core.annotations.Step;
+import starter.pageobjects.RecruitmentPage;
+
+public class RecruitmentCandidateActionSteps extends UIInteractionSteps {
+
+    RecruitmentPage recruitmentPage;
+
+    @Step("Open the OrangeHRM Recruitment page")
+    public void openRecruitmentCPage() {
+        recruitmentPage.open();
+    }
+
+    @Step("Add newCandidate")
+    public void addNewCandidate(String firstName, String lastName, String email, String contact) {
+        recruitmentPage.clickAddCandidate();
+        recruitmentPage.enterFirstName(firstName);
+        recruitmentPage.enterLastname(lastName);
+        recruitmentPage.enterEmail(email);
+        recruitmentPage.enterContactnumber(contact);
+        recruitmentPage.saveCandidate();
+    }
+}
