cd2958f9c3490b82b2f07bb52c401e785aed8dcd
Milanka00 <milankatharangana01@gmail.com>
2024-12-26 15:55:22 +0530

    UIT1: fetaure and step file structure fix

--- src/test/java/starter/steps/ui/RecruitmentCandidateSteps.java
+++ src/test/java/starter/steps/ui/RecruitmentCandidateSteps.java
@@ -1,4 +1,27 @@
 package starter.steps.ui;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import net.thucydides.core.annotations.Steps;
+import starter.actions.RecruitmentCandidateActionSteps;
+
 public class RecruitmentCandidateSteps {
+
+    @Steps
+    RecruitmentCandidateActionSteps recruitmentCandidateActionSteps;
+
+    @Given("I am in the {string} page")
+    public void iAmInThePage(String arg0) {
+        recruitmentCandidateActionSteps.openRecruitmentCPage();
+    }
+
+    @When("I add a new candidate with firstname {string} and lastname {string} and email {string} and contact {string}")
+    public void iAddANewCandidateWithFirstnameAndLastnameAndEmailAndContact(String firstName, String lastName, String email, String contact) {
+        recruitmentCandidateActionSteps.addNewCandidate(firstName, lastName, email, contact);
+    }
+
+    @Then("I should see the candidate \\(testCandidate) {string} in the candidate list")
+    public void iShouldSeeTheCandidateTestCandidateInTheCandidateList(String arg0) {
+    }
 }
--- src/test/resources/features/ui/recuitments_candidates.feature
+++ src/test/resources/features/ui/recuitments_candidates.feature
@@ -4,5 +4,5 @@ Feature: Recruitment management in candidate module
   @loginAsAdmin
   Scenario: Add a new candidate
     Given I am in the "Recruitment" page
-    When I add a new candidate with username "Akash545" and password "test123" and role "ESS"
-    Then I should see the candidate (testUser) "Akash545" in the user list
+    When I add a new candidate with firstname "milanka" and lastname "tharangana" and email "tharangana12@gmail.com" and contact "0711717255"
+    Then I should see the candidate (testCandidate) "milaka" in the candidate list
